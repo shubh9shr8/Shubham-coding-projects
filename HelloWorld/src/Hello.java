38c8426204c2b90733aa1d74d937c82f3bec464c
shubh9shr8 <122762929+shubh9shr8@users.noreply.github.com>
2023-01-16 09:00:14 +0530

    Add files via upload

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,16 @@
+public class Hello {
+
+    public static void main(String[] arg){
+            System.out.println("HelloWorld");
+
+            int MyFirstNumber = (8+15)+(2
+            +7);
+            int MySecNumber = (12);
+            int MyThirdNumber = (6);
+            int MyTotal = MyFirstNumber+MySecNumber+MyThirdNumber;
+            int MyLastOne = (MyTotal-1000);
+        System.out.println(MyLastOne);
+
+    }
+
+}
